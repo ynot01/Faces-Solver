3d89d63973a9936df901d6faac2d1494b10e3794
ynot01 <ynot000001@gmail.com>
2021-10-06 01:24:53 -0400

    Add project files.

---
+++ Faces/Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿
+namespace Faces
+{
+    partial class FormY
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormY));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lastfacetxt = new System.Windows.Forms.Label();
+            this.curface = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.speedInput = new System.Windows.Forms.TextBox();
+            this.noIntErr = new System.Windows.Forms.Label();
+            this.angleDis = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.noNegErr = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            resources.ApplyResources(this.textBox1, "textBox1");
+            this.textBox1.Name = "textBox1";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lastfacetxt
+            // 
+            resources.ApplyResources(this.lastfacetxt, "lastfacetxt");
+            this.lastfacetxt.Name = "lastfacetxt";
+            this.lastfacetxt.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // curface
+            // 
+            resources.ApplyResources(this.curface, "curface");
+            this.curface.Name = "curface";
+            this.curface.Click += new System.EventHandler(this.curface_Click);
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // speedInput
+            // 
+            resources.ApplyResources(this.speedInput, "speedInput");
+            this.speedInput.Name = "speedInput";
+            this.speedInput.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // noIntErr
+            // 
+            resources.ApplyResources(this.noIntErr, "noIntErr");
+            this.noIntErr.Name = "noIntErr";
+            // 
+            // angleDis
+            // 
+            resources.ApplyResources(this.angleDis, "angleDis");
+            this.angleDis.Name = "angleDis";
+            this.angleDis.Click += new System.EventHandler(this.angleDis_Click);
+            // 
+            // pictureBox1
+            // 
+            resources.ApplyResources(this.pictureBox1, "pictureBox1");
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // pictureBox2
+            // 
+            resources.ApplyResources(this.pictureBox2, "pictureBox2");
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.TabStop = false;
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // progressBar1
+            // 
+            resources.ApplyResources(this.progressBar1, "progressBar1");
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Step = 1;
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            // 
+            // trackBar1
+            // 
+            resources.ApplyResources(this.trackBar1, "trackBar1");
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Minimum = 25;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.TickFrequency = 5;
+            this.trackBar1.Value = 100;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label5
+            // 
+            resources.ApplyResources(this.label5, "label5");
+            this.label5.Name = "label5";
+            // 
+            // label6
+            // 
+            resources.ApplyResources(this.label6, "label6");
+            this.label6.Name = "label6";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            resources.ApplyResources(this.label7, "label7");
+            this.label7.Name = "label7";
+            // 
+            // label8
+            // 
+            resources.ApplyResources(this.label8, "label8");
+            this.label8.Name = "label8";
+            // 
+            // label9
+            // 
+            resources.ApplyResources(this.label9, "label9");
+            this.label9.Name = "label9";
+            this.label9.Click += new System.EventHandler(this.label9_Click);
+            // 
+            // label10
+            // 
+            resources.ApplyResources(this.label10, "label10");
+            this.label10.Name = "label10";
+            // 
+            // label11
+            // 
+            resources.ApplyResources(this.label11, "label11");
+            this.label11.Name = "label11";
+            // 
+            // label12
+            // 
+            resources.ApplyResources(this.label12, "label12");
+            this.label12.Name = "label12";
+            // 
+            // noNegErr
+            // 
+            resources.ApplyResources(this.noNegErr, "noNegErr");
+            this.noNegErr.Name = "noNegErr";
+            // 
+            // label13
+            // 
+            resources.ApplyResources(this.label13, "label13");
+            this.label13.Name = "label13";
+            // 
+            // FormY
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.noNegErr);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.angleDis);
+            this.Controls.Add(this.noIntErr);
+            this.Controls.Add(this.speedInput);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.curface);
+            this.Controls.Add(this.lastfacetxt);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormY";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lastfacetxt;
+        private System.Windows.Forms.Label curface;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox speedInput;
+        private System.Windows.Forms.Label noIntErr;
+        private System.Windows.Forms.Label angleDis;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label noNegErr;
+        private System.Windows.Forms.Label label13;
+    }
+}
+
---
+++ Faces/Form1.cs
@@ -0,0 +1,448 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Drawing.Drawing2D;
+using System.Diagnostics;
+
+namespace Faces
+{
+
+    public partial class FormY : Form
+    {
+        public FormY()
+        {
+            InitializeComponent();
+        }
+
+        int dir = 270; //Facing south or towards commander
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            speedInput.Enabled = false;
+            textBox1.Enabled = false;
+            pictureBox1.Image = pictureBox2.Image;
+
+            /*
+            switch (dir % 360)
+            {
+                case 0:
+                    pictureBox1.Image = RotateImage(pictureBox1.Image, -90);
+                    break;
+                case 360:
+                    pictureBox1.Image = RotateImage(pictureBox1.Image, -90);
+                    break;
+                case -360:
+                    pictureBox1.Image = RotateImage(pictureBox1.Image, -90);
+                    break;
+                case 90:
+                    pictureBox1.Image = RotateImage(pictureBox1.Image, 180);
+                    break;
+                case -270:
+                    pictureBox1.Image = RotateImage(pictureBox1.Image, 180);
+                    break;
+                case 180:
+                    pictureBox1.Image = RotateImage(pictureBox1.Image, 90);
+                    break;
+                case -180:
+                    pictureBox1.Image = RotateImage(pictureBox1.Image, 90);
+                    break;
+                default:
+                    break;
+            }
+            */
+
+            dir = 270;
+            float waitln = 500;
+            string finalface = "";
+            string curfaceext = "";
+            curface.Text = "Facing: ";
+            lastfacetxt.Text = "The final face is: ";
+            noIntErr.Visible = false;
+            angleDis.Text = "Angle: " + dir;
+            label6.Text = "Last face: ";
+
+
+
+            try
+            {
+                float parsed = float.Parse(speedInput.Text);
+                if (parsed <= 0){
+                    noNegErr.Visible = true;
+                }
+                else
+                {
+                    waitln = 500 / parsed;
+                }
+            }
+            catch (FormatException)
+            {
+                noIntErr.Visible = true;
+            }
+
+            switch (dir % 360)
+            {
+                case 270:
+                    curfaceext = "Commander";
+                    break;
+                case -90:
+                    curfaceext = "Commander";
+                    break;
+                case 0:
+                    curfaceext = "Left";
+                    break;
+                case 360:
+                    curfaceext = "Left";
+                    break;
+                case -360:
+                    curfaceext = "Left";
+                    break;
+                case 90:
+                    curfaceext = "Away from commander";
+                    break;
+                case -270:
+                    curfaceext = "Away from commander";
+                    break;
+                case 180:
+                    curfaceext = "Right";
+                    break;
+                case -180:
+                    curfaceext = "Right";
+                    break;
+            }
+            curface.Text = "Facing: " + curfaceext;
+
+
+            progressBar1.Maximum = textBox1.Text.Length;
+            progressBar1.Value = 0;
+            progressBar1.Visible = true;
+
+            int loop_position = -1;
+            int facesinloop = 0;
+            foreach (char i in textBox1.Text)
+            {
+                if (i == 'L' || i == 'l' || i == 'R' || i == 'r' || i == 'A' || i == 'a' || i == 'F' || i == 'f')
+                {
+                    facesinloop++;
+                }
+            }
+            label11.Text = "Faces left: " + facesinloop;
+
+            foreach (char i in textBox1.Text)
+            {
+                progressBar1.PerformStep();
+                loop_position++;
+                if (i != 'L' && i != 'l' && i != 'R' && i != 'r' && i != 'A' && i != 'a' && i != 'F' && i != 'f')
+                {
+                    continue;
+                }
+                await Task.Delay((int)waitln);
+                facesinloop--;
+                label11.Text = "Faces left: " + facesinloop;
+                dir %= 360;
+                if (dir == 270 || dir == -90)
+                {
+                    pictureBox1.Image = pictureBox2.Image;
+                }
+
+                char next;
+                try
+                {
+                    bool foundface = false;
+                    int temploop = loop_position;
+
+                    do
+                    {
+                        next = textBox1.Text[temploop + 1];
+
+                        if (next == 'L' || next == 'l')
+                        {
+                            label7.Text = "Next face: Left";
+                            foundface = true;
+                        }
+                        if (next == 'R' || next == 'r')
+                        {
+                            label7.Text = "Next face: Right";
+                            foundface = true;
+                        }
+                        if (next == 'A' || next == 'a')
+                        {
+                            label7.Text = "Next face: About";
+                            foundface = true;
+                        }
+                        if (next == 'F' || next == 'f')
+                        {
+                            label7.Text = "Next face: Front";
+                            foundface = true;
+                        }
+                        temploop++;
+                    } while (foundface == false);
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    label7.Text = "Next face: ";
+                }
+
+                if (i == 'L' || i == 'l') {
+                    label6.Text = "Last face: Left";
+                    dir += 90;
+                    dir %= 360;
+                    pictureBox1.Image = RotateImage(pictureBox1.Image, -90);
+                }
+                if (i == 'R' || i == 'r')
+                {
+                    label6.Text = "Last face: Right";
+                    dir -= 90;
+                    dir %= 360;
+                    pictureBox1.Image = RotateImage(pictureBox1.Image, 90);
+                }
+                if (i == 'A' || i == 'a')
+                {
+                    label6.Text = "Last face: About";
+                    dir -= 180;
+                    dir %= 360;
+                    pictureBox1.Image = RotateImage(pictureBox1.Image, 180);
+                }
+                if (i == 'F' || i == 'f')
+                {
+                    label6.Text = "Last face: Front";
+                    switch (dir % 360)
+                    {
+                        case 0:
+                            pictureBox1.Image = RotateImage(pictureBox1.Image, -90);
+                            break;
+                        case 360:
+                            pictureBox1.Image = RotateImage(pictureBox1.Image, -90);
+                            break;
+                        case -360:
+                            pictureBox1.Image = RotateImage(pictureBox1.Image, -90);
+                            break;
+                        case 90:
+                            pictureBox1.Image = RotateImage(pictureBox1.Image, 180);
+                            break;
+                        case -270:
+                            pictureBox1.Image = RotateImage(pictureBox1.Image, 180);
+                            break;
+                        case 180:
+                            pictureBox1.Image = RotateImage(pictureBox1.Image, 90);
+                            break;
+                        case -180:
+                            pictureBox1.Image = RotateImage(pictureBox1.Image, 90);
+                            break;
+                        default:
+                            break;
+                    }
+                    dir = 270;
+                    dir %= 360; // i know
+                }
+
+
+                switch (dir)
+                {
+                    case 270:
+                        curfaceext = "Commander";
+                        break;
+                    case -90:
+                        curfaceext = "Commander";
+                        break;
+                    case 0:
+                        curfaceext = "Left";
+                        break;
+                    case 360:
+                        curfaceext = "Left";
+                        break;
+                    case -360:
+                        curfaceext = "Left";
+                        break;
+                    case 90:
+                        curfaceext = "Away from commander";
+                        break;
+                    case -270:
+                        curfaceext = "Away from commander";
+                        break;
+                    case 180:
+                        curfaceext = "Right";
+                        break;
+                    case -180:
+                        curfaceext = "Right";
+                        break;
+                }
+                curface.Text = "Facing: " + curfaceext;
+                angleDis.Text = "Angle: " + dir;
+
+
+
+            }
+            progressBar1.Value = progressBar1.Maximum;
+            switch (dir)
+            {
+                case 270:
+                    finalface = "Facing commander";
+                    break;
+                case -90:
+                    finalface = "Facing commander";
+                    break;
+                case 0:
+                    finalface = "Facing left";
+                    break;
+                case 360:
+                    finalface = "Facing left";
+                    break;
+                case -360:
+                    finalface = "Facing left";
+                    break;
+                case 90:
+                    finalface = "Facing away from commander";
+                    break;
+                case -270:
+                    finalface = "Facing away from commander";
+                    break;
+                case 180:
+                    finalface = "Facing right";
+                    break;
+                case -180:
+                    finalface = "Facing right";
+                    break;
+            }
+
+            foreach (char i in textBox1.Text)
+            {
+                if (i == 'L' || i == 'l' || i == 'R' || i == 'r' || i == 'A' || i == 'a' || i == 'F' || i == 'f')
+                {
+                    facesinloop++;
+                }
+            }
+            label11.Text = "Faces left: " + facesinloop;
+
+            lastfacetxt.Text = "The final face is: " + finalface;
+            label7.Text = "Next face: ";
+            progressBar1.Visible = false;
+            button1.Enabled = true;
+            speedInput.Enabled = true;
+            textBox1.Enabled = true;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            noIntErr.Visible = false;
+            noNegErr.Visible = false;
+            if (speedInput.Text != "")
+            {
+                try
+                {
+                    float.Parse(speedInput.Text);
+                    if (float.Parse(speedInput.Text) <= 0)
+                    {
+                        noNegErr.Visible = true;
+                    }
+                }
+                catch (FormatException)
+                {
+                    noIntErr.Visible = true;
+                }
+            }
+        }
+
+        public static Image RotateImage(Image img, float rotationAngle)
+        {
+            //create an empty Bitmap image
+            Bitmap bmp = new Bitmap(img.Width, img.Height);
+
+            //turn the Bitmap into a Graphics object
+            Graphics gfx = Graphics.FromImage(bmp);
+
+            //now we set the rotation point to the center of our image
+            gfx.TranslateTransform((float)bmp.Width / 2, (float)bmp.Height / 2);
+
+            //now rotate the image
+            gfx.RotateTransform(rotationAngle);
+
+            gfx.TranslateTransform(-(float)bmp.Width / 2, -(float)bmp.Height / 2);
+
+            //set the InterpolationMode to HighQualityBicubic so to ensure a high
+            //quality image once it is transformed to the specified size
+            gfx.InterpolationMode = InterpolationMode.NearestNeighbor;
+
+            //now draw our new image onto the graphics object
+            gfx.DrawImage(img, new Point(0, 0));
+
+            //dispose of our Graphics object
+            gfx.Dispose();
+
+            //return the image
+            return bmp;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void curface_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            trackBar1.FindForm().Opacity = (float)trackBar1.Value / 100.0;
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            label8.Text = "Length of input: " + textBox1.Text.Length;
+            int facespresent = 0;
+            foreach (char i in textBox1.Text)
+            {
+                if (i == 'L' || i == 'l' || i == 'R' || i == 'r' || i == 'A' || i == 'a' || i == 'F' || i == 'f')
+                {
+                    facespresent++;
+                }
+            }
+            label9.Text = "Faces in input: " + facespresent;
+            label10.Text = "Non-faces in input: " + (textBox1.Text.Length - facespresent);
+            label11.Text = "Faces left: " + facespresent;
+        }
+
+        private void angleDis_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
